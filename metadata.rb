5a518d00489d8b2f423e0f9d9d3e12de55be9c51
Jason Morgan <jmorgan@f9vs.com>
2016-04-05 12:09:58 -0600

    bumped Version number

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jason.morgan@verizon.com'
 license 'all_rights'
 description 'Installs/Configures bits_server'
 long_description 'Installs/Configures bits_server'
-version '1.0.1'
+version '1.1.0'
 depends 'nuget_dsc', '>= 1.0.7'
 
