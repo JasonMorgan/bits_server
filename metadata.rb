113f1a0be1676ec54fdcc8ac70fa50253bc3899f
Jason Morgan <jmorgan@f9vs.com>
2016-05-14 11:31:12 -0600

    incremented version number, ready to publish.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jason.morgan@verizon.com'
 license 'all_rights'
 description 'Installs/Configures bits_server'
 long_description 'Installs/Configures bits_server'
-version '1.1.0'
+version '1.1.1'
 depends 'nuget_dsc', '>= 1.0.7'
 
